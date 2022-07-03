ecbfbd299daac2e76902e2848617ba5033814140
farzadAB <108418787+FarzadABou@users.noreply.github.com>
2022-07-03 19:17:27 +0430

    Update sin.py

--- sin.py
+++ sin.py
@@ -6,9 +6,4 @@ for i in range(1, 10**9):
     if 0 <= math.sin(i) <= 1/k:
         print(i)
         exit()
- ...
-
-<!---
-FarzadABou/FarzadABou is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+ #  creat by farzadAB . . . . . . . . . . . . . . .
